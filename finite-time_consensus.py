0110ad34267f97b810b41441c49df16ffdddfe97
Yifan Xu <blogerxu@hotmail.com>
2024-04-16 17:40:41 -0500

    First commit - committing all files to repo

---
+++ finite-time_consensus.py
@@ -0,0 +1,14 @@
+# April 16, 2024 Yifan Xu Rice University
+
+# this is some code to help me understand the Consensus problem
+# and the finite-time Consensus property of some graphs. 
+
+# INPUTS:
+
+# an undirected simple graph graph representing the connectivity between the agents
+# assumed to be a connected graph
+GRAPH_TOPOLOGY = [[]] 
+
+# the initial values that the agents hold at t = 0.
+INIT_VALUES = []
+
